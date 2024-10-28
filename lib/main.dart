e6b4993d3ca6d19296447ae1995c0ab6ca9c06b5
rishitha.menusha <rishitha.menush@gmail.com>
2024-10-28 10:58:35 +0530

    Update the main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 void main(){
   runApp(const MyApp());
@@ -14,6 +15,9 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
   }
 }
